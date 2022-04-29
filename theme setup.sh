7c6c112ee45a5e859a307e74bd778c6d23f54838
jma1795 <88217087+jma1795@users.noreply.github.com>
2022-04-29 14:49:25 -0400

    Add files via upload

---
+++ theme setup.sh
@@ -0,0 +1,11 @@
+mkdir .icons
+mkdir .themes
+cd ~/Downloads
+git clone https://github.com/cbrnix/Newaita-reborn.git
+git clone https://github.com/darkomarko42/Elemento.git
+mv Newaita-reborn-master/* ~/.icons 
+mv Elemento/* ~/.themes
+
+rm Newaita-reborn-master
+rm Elemento
+
