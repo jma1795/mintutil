745d171b9fe7e91da618f1c8e51f198c9fffdf0e
jma1795 <88217087+jma1795@users.noreply.github.com>
2022-05-01 19:46:49 -0400

    Update theme setup.sh

--- theme setup.sh
+++ theme setup.sh
@@ -6,13 +6,29 @@ echo '*****************Getting Themes*****************'
 cd ~/Downloads
 git clone https://github.com/cbrnix/Newaita-reborn.git
 git clone https://github.com/darkomarko42/Elemento.git
-git clone https://github.com/varlesh/material-cursors.git
+git clone https://github.com/yeyushengfan258/WinSur-dark-cursors.git
+
 
 mv ~/Downloads/Newaita-reborn/* ~/.icons/ 
 mv ~/Downloads/Elemento/* ~/.themes/
+
+cd WinSur-dark-cursors/
+./install.sh
+
+#Setting Themes
+echo '*****************Auto-setting Themes*****************'
+
+gsettings set org.gnome.desktop.interface gtk-theme 'Elemento Dark'
+gsettings set org.gnome.desktop.interface cursor-theme 'WinSur-dark-cursors'
+sudo flatpak override --filesystem=$HOME/.themes
+gsettings set org.cinnamon.theme name 'Elemento Dark'
+gsettings set org.cinnamon.desktop.interface gtk-theme "Elemento Dark"
+gsettings set org.cinnamon.desktop.wm.preferences theme "Elemento Dark"
+gsettings set org.cinnamon.desktop.interface icon-theme 'Newaita-reborn-dark'
 
 #Cleanup
 echo '*****************Cleaning up*****************'
 rm -rf ~/Downloads/Newaita-reborn
 rm -rf ~/Downloads/Elemento
-
+rm -rf ~/Downloads/WinSur-dark-cursors
+echo '*****************DONE!*****************'
