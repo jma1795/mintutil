4b8ad79dbdbd9387526ed313ae52d73b31e089c5
jma1795 <88217087+jma1795@users.noreply.github.com>
2022-05-11 11:45:09 -0400

    Update theme setup.sh

--- theme setup.sh
+++ theme setup.sh
@@ -6,7 +6,8 @@ mkdir /usr/share/icons
 echo '*****************Downloading Themes*****************'
 cd ~/Downloads
 git clone https://github.com/cbrnix/Newaita-reborn.git
-git clone https://github.com/darkomarko42/Elemento.git
+git clone https://github.com/vinceliuice/Fluent-gtk-theme.git
+
 mkdir Bibata
 cd ~/Downloads/Bibata
 curl -L https://github.com/ful1e5/Bibata_Cursor/releases/download/v1.1.2/Bibata.tar.gz --output ~/Downloads/Bibata.tar.gz
@@ -14,24 +15,24 @@ tar -xf Bibata.tar.gz
 cd ~/Downloads
 echo '*****************Installing Themes*****************'
 mv ~/Downloads/Newaita-reborn/* /usr/share/icons/ 
-mv ~/Downloads/Elemento/* /usr/share/themes/
 mv ~/Downloads/Bibata/* /usr/share/icons/ 
-
+cd ~/Downloads/Fluent-gtk-theme
+bash install.sh
 
 #Setting Themes
 echo '*****************Attempting to Auto-setting Themes*****************'
 
-gsettings set org.gnome.desktop.interface gtk-theme 'Elemento Dark'
+gsettings set org.gnome.desktop.interface gtk-theme 'Fluent-dark'
 gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Origonal-Classic'
 sudo flatpak override --filesystem=$HOME/.themes
-gsettings set org.cinnamon.theme name 'Elemento Dark'
-gsettings set org.cinnamon.desktop.interface gtk-theme "Elemento Dark"
-gsettings set org.cinnamon.desktop.wm.preferences theme "Elemento Dark"
-gsettings set org.cinnamon.desktop.interface icon-theme 'Newaita-reborn-dark'
+gsettings set org.cinnamon.theme name 'Fluent-dark'
+gsettings set org.cinnamon.desktop.interface gtk-theme "Fluent-dark"
+gsettings set org.cinnamon.desktop.wm.preferences theme "Fluent-dark"
+gsettings set org.cinnamon.desktop.interface icon-theme 'Newaita-reborn-yellow-dark'
 
 #Cleanup
 echo '*****************Cleaning up*****************'
 rm -rf ~/Downloads/Newaita-reborn
-rm -rf ~/Downloads/Elemento
+rm -rf ~/Downloads/Fluent-gtk-theme
 rm -rf ~/Downloads/Bibata
 echo '*****************DONE!*****************'
