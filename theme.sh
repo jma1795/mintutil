43142f6e86d443dee535d555b2b81dd10702726b
jma1795 <88217087+jma1795@users.noreply.github.com>
2022-05-17 07:54:10 -0400

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -5,19 +5,19 @@ mkdir ~/.themes
 #Get Themes
 echo '*****************Downloading Themes*****************'
 cd ~/Downloads
-echo '*****************Getting Newaita Icons*****************'
-git clone https://github.com/cbrnix/Newaita.git
-echo '*****************Getting Elemento GTK-Theme (all Colors)*****************'
-git clone https://github.com/darkomarko42/Elemento.git
-git clone https://github.com/darkomarko42/Elemento-slate.git
-git clone https://github.com/darkomarko42/Elemento-Strawberry.git
-git clone https://github.com/darkomarko42/Elemento-cocoa.git
-git clone https://github.com/darkomarko42/Elemento-Orange.git
-git clone https://github.com/darkomarko42/Elemento-grape.git
-git clone https://github.com/darkomarko42/Elemento-bubblegum.git
-git clone https://github.com/darkomarko42/Elemento-mint.git
-git clone https://github.com/darkomarko42/Elemento-Lime.git
-git clone https://github.com/darkomarko42/Elemento-Banana.git
+echo '*****************Getting Icons*****************'
+git clone https://github.com/darkomarko42/Marwaita-Icons
+echo '*****************Getting Themes*****************'
+git clone https://github.com/darkomarko42/Marwaita.git
+git clone https://github.com/darkomarko42/Marwaita-manjaro
+git clone https://github.com/darkomarko42/Marwaita-EndeavourOs
+git clone https://github.com/darkomarko42/Marwaita-ubuntu
+git clone https://github.com/darkomarko42/Marwaita-Zorin
+git clone https://github.com/darkomarko42/Marwaita-Pop_os
+git clone https://github.com/darkomarko42/Marwaita-Debian
+git clone https://github.com/darkomarko42/Marwaita-peppermint
+git clone https://github.com/darkomarko42/Marwaita-Mint
+git clone https://github.com/darkomarko42/Marwaita-OSX
 echo '*****************Getting Bibata Cursors*****************'
 mkdir Bibata
 cd ~/Downloads/Bibata
@@ -25,34 +25,32 @@ curl -L https://github.com/ful1e5/Bibata_Cursor/releases/download/v1.1.2/Bibata.
 tar -xf Bibata.tar.gz
 
 cd ~/Downloads
-#git clone https://github.com/AdisonCavani/distro-grub-themes.git
 
 echo '*****************Installing Icons*****************'
 mv ~/Downloads/Newaita/* ~/.icons/ 
-sudo mv ~/Downloads/Bibata/* /usr/share/icons
+sudo mv ~/Downloads/Bibata/* /usr/share/icons/
 echo '*****************Installing Themes*****************'
-#mv ~/Downloads/distro-grub-themes ~/
-mv ~/Downloads/Elemento/* ~/.themes/
-mv ~/Downloads/Elemento-slate/* ~/.themes/
-mv ~/Downloads/Elemento-Strawberry/* ~/.themes/
-mv ~/Downloads/Elemento-cocoa/* ~/.themes/
-mv ~/Downloads/Elemento-Orange/* ~/.themes/
-mv ~/Downloads/Elemento-grape/* ~/.themes/
-mv ~/Downloads/Elemento-bubblegum/* ~/.themes/
-mv ~/Downloads/Elemento-mint/* ~/.themes/
-mv ~/Downloads/Elemento-Lime/* ~/.themes/
-mv ~/Downloads/Elemento-Banana/* ~/.themes/
+mv ~/Downloads/Marwaita/* ~/.themes/
+mv ~/Downloads/Marwaita-manjaro/* ~/.themes/
+mv ~/Downloads/Marwaita-EndeavourOs/* ~/.themes/
+mv ~/Downloads/Marwaita-ubuntu/* ~/.themes/
+mv ~/Downloads/Marwaita-Zorin/* ~/.themes/
+mv ~/Downloads/Marwaita-Pop_os/* ~/.themes/
+mv ~/Downloads/Marwaita-Debian/* ~/.themes/
+mv ~/Downloads/Marwaita-peppermint/* ~/.themes/
+mv ~/Downloads/Marwaita-Mint/* ~/.themes/
+mv ~/Downloads/Marwaita-OSX/* ~/.themes/
 
 #Setting Themes
 echo '*****************Attempting to Auto-set Theme*****************'
 
-gsettings set org.gnome.desktop.interface gtk-theme 'Elemento-dark'
+gsettings set org.gnome.desktop.interface gtk-theme 'Marwaita-dark'
 gsettings set org.gnome.desktop.interface cursor-theme 'Bibata-Modern-Classic'
 sudo flatpak override --filesystem=$HOME/.themes
 gsettings set org.cinnamon.theme name 'Elemento-dark'
-gsettings set org.cinnamon.desktop.interface gtk-theme "Elemento-dark"
-gsettings set org.cinnamon.desktop.wm.preferences theme "Elemento-dark"
-gsettings set org.cinnamon.desktop.interface icon-theme 'Newaita-dark'
+gsettings set org.cinnamon.desktop.interface gtk-theme "Marwaita-dark"
+gsettings set org.cinnamon.desktop.wm.preferences theme "Marwaita-dark"
+gsettings set org.cinnamon.desktop.interface icon-theme 'Marwaita-dark'
 
 #Cleanup
 echo '*****************Cleaning up*****************'
