9cb51d755a0765a421d1168aec44ccef9c879859
jma1795 <88217087+jma1795@users.noreply.github.com>
2022-06-04 18:06:15 -0400

    Update theme.sh
    Cleaned up downloaded themes and icons.
    
    Update to Pop OS icons

--- theme.sh
+++ theme.sh
@@ -6,18 +6,16 @@ mkdir ~/.themes
 echo '*****************Downloading Themes*****************'
 cd ~/Downloads
 echo '*****************Getting Icons*****************'
-git clone https://github.com/gusbemacbe/suru-plus.git
+git clone https://github.com/pop-os/icon-theme pop-icon-theme
+cd pop-icon-theme
+meson build
+sudo ninja -C "build" install
+cd ..
 echo '*****************Getting Themes*****************'
 git clone https://github.com/darkomarko42/Marwaita.git
-git clone https://github.com/darkomarko42/Marwaita-manjaro
 git clone https://github.com/darkomarko42/Marwaita-EndeavourOs
-git clone https://github.com/darkomarko42/Marwaita-ubuntu
-git clone https://github.com/darkomarko42/Marwaita-Zorin
 git clone https://github.com/darkomarko42/Marwaita-Pop_os
-git clone https://github.com/darkomarko42/Marwaita-Debian
-git clone https://github.com/darkomarko42/Marwaita-peppermint
 git clone https://github.com/darkomarko42/Marwaita-Mint
-git clone https://github.com/darkomarko42/Marwaita-OSX
 echo '*****************Getting Bibata Cursors*****************'
 mkdir Bibata
 cd ~/Downloads/Bibata
@@ -26,33 +24,20 @@ tar -xf Bibata.tar.gz
 cd ~/Downloads
 
 echo '*****************Installing Icons*****************'
-mv ~/Downloads/suru-plus/* ~/.icons/ 
 sudo mv ~/Downloads/Bibata/* /usr/share/icons/
 
 echo '*****************Installing Themes*****************'
 mv ~/Downloads/Marwaita/* ~/.themes/
-mv ~/Downloads/Marwaita-manjaro/* ~/.themes/
 mv ~/Downloads/Marwaita-EndeavourOs/* ~/.themes/
-mv ~/Downloads/Marwaita-ubuntu/* ~/.themes/
-mv ~/Downloads/Marwaita-Zorin/* ~/.themes/
 mv ~/Downloads/Marwaita-Pop_os/* ~/.themes/
-mv ~/Downloads/Marwaita-Debian/* ~/.themes/
-mv ~/Downloads/Marwaita-peppermint/* ~/.themes/
 mv ~/Downloads/Marwaita-Mint/* ~/.themes/
-mv ~/Downloads/Marwaita-OSX/* ~/.themes/
 
 #Cleanup
 echo '*****************Cleaning up*****************'
-rm -rf ~/Downloads/suru-plus/
+rm -rf ~/Downloads/pop-icon-theme/
 rm -rf ~/Downloads/Bibata/
 rm -rf ~/Downloads/Marwaita/
-rm -rf ~/Downloads/Marwaita-manjaro/
 rm -rf ~/Downloads/Marwaita-EndeavourOs/
-rm -rf ~/Downloads/Marwaita-ubuntu/
-rm -rf ~/Downloads/Marwaita-Zorin/
 rm -rf ~/Downloads/Marwaita-Pop_os/
-rm -rf ~/Downloads/Marwaita-Debian/
-rm -rf ~/Downloads/Marwaita-peppermint/
 rm -rf ~/Downloads/Marwaita-Mint/
-rm -rf ~/Downloads/Marwaita-OSX/
 echo '*****************DONE!*****************'
