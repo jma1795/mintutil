e5db1691c4e959b9becfc6cbea841aa8d59df563
jma1795 <88217087+jma1795@users.noreply.github.com>
2022-06-28 08:31:16 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,39 +4,57 @@ function pause(){
    read -p "$*"
 }
 ################################################################ 
-PS3='Setting up Joecinnamon'
-foods=("Pre-Setup" "Appearance" "Packages" "Quit")
+PS3='What Package do you want to install:'
+foods=("Pre-Setup" "Appearance" "Packages" "Optimizations" "Quit")
 select fav in "${foods[@]}"; do
     case $fav in
         "Pre-Setup")
             echo "****Installing $fav git Package****"
 	    # run package installer
 bash Pre-setup.sh
-#*****************************************************************
             echo "****Packages Installed****"
+pause 'Press [Enter] key to continue...'
+bash setup.sh
+#*****************************************************************
+
             ;;
         "Appearance")
             echo "****Setting up $fav****"
 	    # run package installer
 bash theme.sh
+            echo "****Theme Installed****"
+pause 'Press [Enter] key to continue...'
+bash setup.sh
 #*****************************************************************
-            echo "****Packages Installed****"
+            
             ;;
         "Packages")
             echo "****Installing $fav and Apps****"
 	    # run package installer
 bash packages.sh
+echo "****Packages Installed****"
+pause 'Press [Enter] key to continue...'
+bash setup.sh
 #*****************************************************************
-            echo "****Packages and Apps Installed****"
+            ;;
+        "Optimizations")
+            echo "****Installing $fav****"
+	    # run package installer
+bash optimize.sh
+echo "****System Optimized****"
+pause 'Press [Enter] key to continue...'
+bash setup.sh
+#*****************************************************************
+
+#*****************************************************************
 	    break
             ;;
 	"Quit")
 	    echo "*****Script Terminated****"
         echo "*****System Reboot Suggested****"
 	    exit
+done
 	    ;;
         *) echo "invalid option $REPLY";;
     esac
-pause 'Press [Enter] key to continue...'
-exit
 done
