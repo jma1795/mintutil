b584784d93bf83551705be87b6159177b1a69d7f
jma1795 <88217087+jma1795@users.noreply.github.com>
2022-06-28 08:30:57 -0400

    Update Pre-setup.sh

--- Pre-setup.sh
+++ Pre-setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # init
-
+sudo apt -y update
 sudo apt install -y git make inkscape
 
 cd apps
-sudo apt install ./nala-legacy_0.8.2_amd64.deb
-sudo apt install ./system-monitoring-center_1.21.0_all.deb
+sudo apt install -y ./nala-legacy_0.8.2_amd64.deb
+sudo apt install -y ./system-monitoring-center_1.21.0_all.deb
 echo "****Packages Installed****"
-
