5a224abcd2270135d9a20a058327fb426f28bac8
jma1795 <88217087+jma1795@users.noreply.github.com>
2022-07-01 16:22:09 -0400

    Update optimize.sh

--- optimize.sh
+++ optimize.sh
@@ -13,19 +13,40 @@ OnCalendar=daily' >> /etc/systemd/system/fstrim.timer.d/override.conf
 
 # Reduce the inclination to swap
 cat /proc/sys/vm/swappiness
-pause 'The default value is 60, however you should change it to 25 for SSDs. Press [Enter] key to continue...'
+echo 'The default value is 60 for HDDs, setting to 25 for SSDs'
 
 echo '# Reduce the inclination to swap
 vm.swappiness=25' >> /etc/sysctl.conf
 
 
 #Optional: set noatime
+echo 'setting noatime for SSDs'
 sudo sed -i 's/ errors=remount-ro/ noatime,errors=remount-ro/' /etc/fstab
 echo "****Task Complete a System Reboot is suggested****"
 
 pause 'Press [Enter] key to continue...'
 #*****************************************************************
-            echo "****Packages Installed****"
+
+
+#Optional: set better sound profile
+sudo cp /etc/pulse/daemon.conf /etc/pulse/daemon.conf.backup
+
+echo '; default-sample-format = float32le
+; default-sample-rate = 48000
+; alternate-sample-rate = 44100
+; default-sample-channels = 2
+; default-channel-map = front-left,front-right
+; default-fragments = 2
+; default-fragment-size-msec = 125
+; resample-method = speex-float-10
+; enable-lfe-remixing = no
+; high-priority = yes
+; nice-level = -11
+; realtime-scheduling = yes
+; realtime-priority = 9
+; rlimit-rtprio = 9' >> /etc/pulse/daemon.conf
+
+            echo "****Optimization Complete!****"
 	    # run package installer
 bash setup.sh
 #*****************************************************************
