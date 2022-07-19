8b1f0465d0500bafa63fb244f3e06782d9dbf5c9
jma1795 <88217087+jma1795@users.noreply.github.com>
2022-07-19 08:32:12 -0400

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -3,21 +3,22 @@ sudo apt -y update && apt -y upgrade
 
 
 #Native packages
-nala install cheese neofetch powertop evolution
+nala install cheese neofetch
 
 
 #Flatpaks
 flatpak install -y flathub com.google.Chrome
+flatpak install -y flathub org.gnome.Evolution
 flatpak install -y flathub com.sindresorhus.Caprine
 flatpak install -y flathub com.discordapp.Discord
 flatpak install -y flathub com.spotify.Client
 flatpak install -y flathub net.cozic.joplin_desktop
+flatpak install -y flathub com.github.tchx84.Flatseal
+sudo flatpak override --filesystem=$HOME/.themes
 
 #terminal
-echo '#POWERLINE
-powerline-daemon -q
-POWERLINE_BASH_CONTINUATION=1
-POWERLINE_BASH_SELECT=1
-source /usr/share/powerline/bindings/bash/powerline.sh' >> ~/.bashrc
-
-
+echo '#POWERLINE' >> ~/.bashrc
+echo 'powerline-daemon -q' >> ~/.bashrc
+echo 'POWERLINE_BASH_CONTINUATION=1' >> ~/.bashrc
+echo 'POWERLINE_BASH_SELECT=1' >> ~/.bashrc
+echo 'source /usr/share/powerline/bindings/bash/powerline.sh' >> ~/.bashrc
