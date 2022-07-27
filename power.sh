fc0d1c6516b716a5cfde1038e6fb790e569c7bd2
jma1795 <88217087+jma1795@users.noreply.github.com>
2022-07-26 20:51:00 -0400

    Update power.sh

--- power.sh
+++ power.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 # init
 
+sudo nala update
 sudo nala install -y tlp tp-smapi-dkms acpi-call-dkms smartmontools linux-tools-generic powertop
 
-mv ~/Downloads/joecinnamon-main/power/power /etc/pm/power.d/power
+sudo tlp start
+sudo systemctl enable tlp --now
+
+sudo powertop --auto-tune
+
+cat << EOF | sudo tee /etc/systemd/system/powertop.service
+[Unit]
+Description=PowerTOP auto tune
+
+[Service]
+Type=oneshot
+Environment="TERM=dumb"
+RemainAfterExit=true
+ExecStart=/usr/sbin/powertop --auto-tune
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable powertop.service
