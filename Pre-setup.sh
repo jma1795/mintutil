cf226a9c95dab0f828d64ade511883cab988d2f7
jma1795 <88217087+jma1795@users.noreply.github.com>
2022-07-26 20:57:31 -0400

    Update Pre-setup.sh

--- Pre-setup.sh
+++ Pre-setup.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 # init
 sudo apt -y update
-sudo apt install -y git make inkscape
+sudo apt install -y git make
 
-cd apps
-sudo apt install -y ./nala-legacy_0.8.2_amd64.deb
-sudo apt install -y ./system-monitoring-center_1.21.0_all.deb
-echo "****Packages Installed****"
+#Volian Scar Repo for Nala APT Front-end
+echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
+wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
+
+#Ubuntu 22.04 / Debian Sid
+#sudo apt update && sudo apt install nala
+#Ubuntu 21.04 / Debian Stable
+sudo apt update && sudo apt install nala-legacy
+
+
+echo "****Pre-setup Complete****"
