b35437cbac63c15f43d82e347e66c58244cd99d1
jma1795 <88217087+jma1795@users.noreply.github.com>
2022-08-14 16:37:39 -0400

    Add files via upload

---
+++ theme.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# init
+#installing icon theme
+cd icons
+git clone https://github.com/masalla-art/masalla-icon-theme.git
+cd masalla-icon-theme/
+sudo cp -R ./masalla/ /usr/share/icons/
+sudo cp -R ./masalla-dark/ /usr/share/icons/
+cd ..
+
+git clone https://github.com/masalla-art/masalla-colors.git
+sudo cp -R ./masalla-red/ /usr/share/icons/
+sudo cp -R ./masalla-red-dark/ /usr/share/icons/
+sudo cp -R ./masalla-yellow/ /usr/share/icons/
+sudo cp -R ./masalla-yellow-dark/ /usr/share/icons/
+sudo cp -R ./masalla-grey/ /usr/share/icons/
+sudo cp -R ./masalla-grey-dark/ /usr/share/icons/
+sudo cp -R ./masalla-green/ /usr/share/icons/
+sudo cp -R ./masalla-green-dark/ /usr/share/icons/
+sudo cp -R ./masalla-violet/ /usr/share/icons/
+sudo cp -R ./masalla-violet-dark/ /usr/share/icons/
+sudo cp -R ./masalla-brown/ /usr/share/icons/
+sudo cp -R ./masalla-brown-dark/ /usr/share/icons/
+sudo cp -R ./masalla-black/ /usr/share/icons/
+sudo cp -R ./masalla-black-dark/ /usr/share/icons/
+cd ..
+
+#installing gtk/cinnamon theme
+cd theme
+git clone https://github.com/hrdwrrsk/adementary-theme.git
+cd adementary-theme
+sudo ./install.sh
+cd ..
+cd ..
+
+flatpak install flathub org.gtk.Gtk3theme.Adementary
