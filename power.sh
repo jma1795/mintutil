891a212f50c1a81e1f48c69dbfbdf57e497e8b0f
jma1795 <88217087+jma1795@users.noreply.github.com>
2022-08-31 15:37:52 -0400

    Update power.sh

--- power.sh
+++ power.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 # init
-sudo apt-add-repository -ys ppa:system76-dev/stable
 
-sudo nala update
-sudo nala install -y system76-power system76-io-dkms system76-dkms system76-acpi-dkms powertop
-sudo powertop --auto-tune
+# Install the packages
+sudo nala install tlp tlp-rdw tp-smapi-dkms acpi-call-dkms linux-cpupower smartmontools gsmartcontrol smart-notifier mailutils
 
-#Ucomment Below if using LMDE or any Debian-based systems
-#https://launchpad.net/~system76-dev/+archive/ubuntu/stable/+files/system76-power_1.1.22~1659403314~20.04~357f4b0~dev_amd64.deb
-
+#Install TLPUI
+flatpak install flathub com.github.d4nj1.tlpui
