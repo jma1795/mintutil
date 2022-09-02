4e837727ea052bab0cc1873bbe60164ccaf357e9
jma1795 <88217087+jma1795@users.noreply.github.com>
2022-09-02 17:35:52 -0400

    Update power.sh

--- power.sh
+++ power.sh
@@ -3,6 +3,6 @@
 
 # Install the packages
 sudo nala install tlp tlp-rdw tp-smapi-dkms acpi-call-dkms linux-cpupower smartmontools gsmartcontrol smart-notifier mailutils
-
+sudo tlp start
 #Install TLPUI
 flatpak install flathub com.github.d4nj1.tlpui
