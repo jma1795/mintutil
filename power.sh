5980149155c1cf6840a0e7f6f82c1639a9532bcc
jma1795 <88217087+jma1795@users.noreply.github.com>
2023-01-04 16:56:15 -0500

    Update power.sh

--- power.sh
+++ power.sh
@@ -2,25 +2,18 @@
 # init
 
 # Install the packages
-#sudo nala install tlp tlp-rdw tp-smapi-dkms acpi-call-dkms smartmontools gsmartcontrol smart-notifier mailutils powertop
-sudo nala install powertop
+sudo nala install tlp tlp-rdw cpufrequtils powertop
+
 cd config
-#cp tlp.conf /etc/tlp.conf
+cp tlp.conf /etc/tlp.conf
 sudo cp powertop.service /etc/systemd/system/powertop.service
 cd ..
-#sudo systemctl enable tlp.service
+sudo systemctl enable tlp.service
+sudo systemctl daemon-reload
 sudo systemctl enable powertop.service
-#sudo tlp start
+
+sudo tlp start
 sudo powertop --auto-tune
 
 #Install TLPUI
-#flatpak install flathub com.github.d4nj1.tlpui
-
-#Auto-cpufreq install:
-git clone https://github.com/AdnanHodzic/auto-cpufreq.git
-cd auto-cpufreq && sudo ./auto-cpufreq-installer
-sudo auto-cpufreq --install
-
-cd config
-sudo cp auto-cpufreq.conf /etc/auto-cpufreq.conf
-cd ..
+flatpak install flathub com.github.d4nj1.tlpui
