fb95e65718bd6a7d1bc1280d6bbde250d5f78304
jma1795 <88217087+jma1795@users.noreply.github.com>
2023-01-13 18:00:55 -0500

    Update power.sh

--- power.sh
+++ power.sh
@@ -2,18 +2,15 @@
 # init
 
 # Install the packages
-sudo nala install tlp tlp-rdw cpufrequtils powertop
+sudo nala install tlp tlp-rdw powertop acpi-call-dkms software-properties-common
 
-cd config
-cp tlp.conf /etc/tlp.conf
+sudo powertop calibrate
+
 sudo cp powertop.service /etc/systemd/system/powertop.service
-cd ..
-sudo systemctl enable tlp.service
-sudo systemctl daemon-reload
-sudo systemctl enable powertop.service
+sudo cp tlp.conf
 
-sudo tlp start
-sudo powertop --auto-tune
+systemctl enable --now powertop
+systemctl enable --now tlp.service
 
 #Install TLPUI
 flatpak install flathub com.github.d4nj1.tlpui
