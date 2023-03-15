c8ac580932e30103979d9115b5fc68bfb0d7c68f
jma1795 <88217087+jma1795@users.noreply.github.com>
2023-03-15 19:29:36 -0400

    Update joemint.sh

--- joemint.sh
+++ joemint.sh
@@ -11,43 +11,43 @@ setup=("Pre-Setup" "Software" "Power Management" "Security" "Audio" "Appearance"
 select fav in "${setup[@]}"; do
     case $fav in
         "Pre-Setup")
-            echo "Installing prerequisitis"
+            echo "***Installing prerequisitis***"
 	    #Run Script
         bash pre-setup.sh
             ;;
         "Software")
-            echo "Running $fav script"
+            echo "***Running $fav script***"
 	    #Run Script
         bash packages.sh
             ;;
         "Power Management")
-            echo "Running $fav script"
+            echo "***Running $fav script***"
 	    #Run Script
         bash power.sh
 	    break
             ;;
         "Security")
-            echo "Running $fav script"
+            echo "***Running $fav script***"
 	    #Run Script
         bash power.sh
 	    break
             ;;
         "Audio")
-            echo "Running $fav script"
+            echo "***Running $fav script***"
 	    #Run Script
         bash pipewire.sh
 	    break
             ;;
         "Appearance")
-            echo "Running $fav script"
+            echo "***Running $fav script***"
 	    #Run Script
         bash theme.sh
 	    break
             ;;
 	"Quit")
-	    echo "Program Terminated"
+	    echo "***Program Terminated***"
 	    exit
 	    ;;
-        *) echo "invalid option $REPLY";;
+        *) echo "***invalid option $REPLY***";;
     esac
 done
